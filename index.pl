6690cf0c81173e8e72bea03362068e7511430d6c
leliveld <leliveld@ISCNPC0K6GPD>
2017-09-02 14:29:07 +0200

    .

--- index.pl
+++ index.pl
@@ -7,8 +7,9 @@ round(10.2) == 10 || die "round(10.2) != 10";
 print "Content-type: text/html\n\n";
 print <<EOF
 <html>
-<head><title>Everything is OK</title></head>
+<head><title>MIKE: Everything is OK</title></head>
 <body>
+<H1> Here from MIKE</H1>
 Everything is fine.
 </body>
 </html>
