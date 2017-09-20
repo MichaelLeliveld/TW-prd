4c7254d7cc0f3a6283fe00812e6bde92c8a4041c
mike <mike@home.de>
2017-09-20 17:42:18 +0200

    .

--- index.pl
+++ index.pl
@@ -9,15 +9,20 @@ use LWP::Protocol::https;
 
 print "Content-type: text/html\n\n";
 
-my $thisUrl = 'http://prd-aml-tradewatch.7e14.starter-us-west-2.openshiftapps.com/';
-
 my %pars     = CGI::Vars();
 my $user     = $pars{user};
 my $password = $pars{password};
+my $path     = $pars{path};
+$path = 'index.pl' unless defined($path);
+
 if ( defined($user) && defined($password) ) {
-	my $ua   = LWP::UserAgent->new();
-	my $host = "tradewatch.hopto.org";
-	my $url  = "http://$host/index.pl";
+	my $ua          = LWP::UserAgent->new();
+	my $host        = "tradewatch.hopto.org";
+	my $url         = "http://$host/$path";
+	my $thisHost    = 'prd-aml-tradewatch.7e14.starter-us-west-2.openshiftapps.com';
+	my $thisUrl     = "http://$host/?path=";
+	my $credentials = "&user=$user&password=$password";
+
 	$ua->credentials( "$host:80", 'TradeWatch', $user, $password );
 
 	my $response = undef;
@@ -25,7 +30,7 @@ if ( defined($user) && defined($password) ) {
 	ysloaderMonitoring::error($@) if $@;
 	if ( $response->is_success ) {
 		my $html = $response->decoded_content;
-		$html =~ s/<\s*[Aa]\s+[Hh][Rr][Ee][Ff]=\"([^>]+)\"\s*>/<a href=\"$thisUrl$1\">/g;
+		$html =~ s/<\s*[Aa]\s+[Hh][Rr][Ee][Ff]=\"([^>]+)\"\s*>/<a href=\"$thisUrl$1$credentials\">/g;
 		print $html;
 	}
 	else {
