961a218966934be16bd6c2a12fd0904743b4cbf2
mike <mike@home.de>
2017-09-20 14:54:22 +0200

    .

--- index.pl
+++ index.pl
@@ -1,19 +1,7 @@
 #!/usr/bin/perl
 
-use Math::Round;
-
-round(10.2) == 10 || die "round(10.2) != 10";
+use LWP::UserAgent;
+use LWP::Protocol::https;
 
 print "Content-type: text/html\n\n";
-print <<EOF
-<html>
-<head><title>MIKE: Everything is OK</title></head>
-<body>
-
-<H1>Here from MIKE</H1>
-Everything is fine.
-
-</body>
-</html>
-EOF
-;
+print "<H2>TEST</H2>";
